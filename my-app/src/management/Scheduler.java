1d6d123871244fe2e90734c6977440c52303522f
AdamSBobak <asb14@rice.edu>
2022-09-24 23:23:05 -0500

    Expanding Scheduler logic -> Now Schedule Blocking

--- my-app/src/management/Scheduler.java
+++ my-app/src/management/Scheduler.java
@@ -3,6 +3,7 @@ package management;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Map;
+import java.awt.Point;
 
 import database.item.Employee;
 import database.item.Job;
@@ -14,6 +15,7 @@ public class Scheduler {
     // Location: String
     // Time needed: Integer (in hours)
     // Details: Arbitrary String
+
     // Employee
     // Phone number: String
     // Skills: Array of strings
@@ -28,8 +30,38 @@ public class Scheduler {
 
 
     private Map scheduleAll(ArrayList<Employee> empList, ArrayList<Job> jobList){
+        int numEmps = empList.size();
+        int[] empDist = new int[numEmps];
+        for (int i = 0; i < numEmps; i++){
+            empDist[i] = -1;
+        }
+        for (Job emptyJob : jobList){
+            boolean jobScoped = false;
+            int distToler = 0;
+            while (jobScoped != true){
+                distToler++;
+                for (int i = 0; i < numEmps; i++){
+
+                    if (empList.get(i).getSkills().containsAll(emptyJob.getSkills()) && radialDist() <= distToler){
+                        emptyJob.setAssignedEmployee(empList.get(i));
+                        jobScoped = true;
+                    }
+                }
+                if (distToler > 100){
+                    jobScoped = true;
+                }
+            }
+        }
         
         return null;
+    }
+
+    private double radialDist(){
+
+    }
+
+    private ArrayList<Point> scheduleParse(Schedule schedule){
+        for
     }
 
     
