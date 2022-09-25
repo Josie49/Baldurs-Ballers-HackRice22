172d48f3b576a38782e3a1ec379cc94c5e5d2c5c
AdamSBobak <asb14@rice.edu>
2022-09-24 22:20:05 -0500

    Made simple schedule and expanded scheduler

--- my-app/src/management/Schedule.java
+++ my-app/src/management/Schedule.java
@@ -1,4 +1,13 @@
 package management;
 
 public class Schedule {
+
+    public boolean[] timeTable;
+
+    public Schedule(){
+        timeTable = new boolean[24];
+        for (int i = 0; i < 24; i++){
+            timeTable[i] = false;
+        }
+    }
 }
--- my-app/src/management/Scheduler.java
+++ my-app/src/management/Scheduler.java
@@ -1,21 +1,35 @@
 package management;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Map;
+
+import database.item.Employee;
+import database.item.Job;
+
 public class Scheduler {
 
     // Work order
-    // Skills: String (To be parsed)
+    // Skills: Array of Strings
     // Location: String
     // Time needed: Integer (in hours)
     // Details: Arbitrary String
     // Employee
     // Phone number: String
-    // Skills: String (To be parsed)
+    // Skills: Array of strings
     // Availability: 2 separate integers
     // Starting Location: String
 
 
     public Scheduler() {
 
+    }
+
+
+
+    private Map scheduleAll(ArrayList<Employee> empList, ArrayList<Job> jobList){
+        
+        return null;
     }
 
     
