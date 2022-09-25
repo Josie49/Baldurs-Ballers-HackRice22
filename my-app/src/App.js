908b6b0ec2dda010c6f0f2e02b6cf8610aed4c23
Jose Mata Esqueda <jgm7@rice.edu>
2022-09-24 19:17:15 -0500

    Added new dependencies and libraries

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+import WorkOrderPage from './pages/WorkOrderPage';
+
+function Home() {
+    return (
+        <div>
+            <h1>Welcome to Baldur's Ballers v2</h1>
+        </div>
+    );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <div>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/WorkOrderPage">Submit A Work Order</NavLink>
+        </div>
+
+        <Routes>
+            <Route exact path="/WorkOrderPage" element={<WorkOrderPage />} />
+
+            <Route exact path="/" element={<Home />}/>
+        </Routes>
+    </Router>
   );
 }
 
