f97a1a12cd1eb29e83d423014d288b5470f58671
Phoebe Scaccia <phoebescaccia@gmail.com>
2022-09-24 19:25:22 -0500

    Create the skills enum

--- my-app/src/database/item/Skills.java
+++ my-app/src/database/item/Skills.java
@@ -1,4 +1,45 @@
 package database.item;
 
 public enum Skills {
+    CARPENTRY {
+        @Override
+        public String toString() {
+            return "Carpentry";
+        }
+    }, BACKEND {
+        @Override
+        public String toString() {
+            return "Back-end support";
+        }
+    }, UI {
+        @Override
+        public String toString() {
+            return "UI design";
+        }
+    }, HARDWARE {
+        @Override
+        public String toString() {
+            return "Hardware support";
+        }
+    }, EMOTIONAL {
+        @Override
+        public String toString() {
+            return "Emotional support";
+        }
+    }, WIRING {
+        @Override
+        public String toString() {
+            return "Wiring";
+        }
+    }, CONSULTATION {
+        @Override
+        public String toString() {
+            return "Consultation";
+        }
+    }, NOTARY {
+        @Override
+        public String toString() {
+            return "Notary";
+        }
+    }
 }
